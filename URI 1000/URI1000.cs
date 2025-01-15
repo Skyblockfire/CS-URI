b43716195824a7cb272c78b7d78918c94ae204b6
As1mov <realmofthemadzueira@gmail.com>
2025-01-15 15:31:12 -0300

    feat:adiciona o URI1000.

---
+++ URI 1000/URI1000.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//Realized by Matt
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        //Just a Basic Hello World here, CURSED REMOVED.
+        Console.WriteLine("Hello World!");
+
+    }
+
+}
