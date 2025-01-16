1211a678145367ab914f9ed4c0850163648853e9
As1mov <realmofthemadzueira@gmail.com>
2025-01-15 22:10:46 -0300

    feat:adicionar o URI1001.

---
+++ URI 1001/URI 1001.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//Realized by Matt
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        //Code for simple sum.
+        int A, B, X;
+
+        A = Convert.ToInt32(Console.ReadLine());
+        B = Convert.ToInt32(Console.ReadLine());
+
+        X = A + B;
+
+        Console.WriteLine("X = " + X);
+
+    }
+
+}
