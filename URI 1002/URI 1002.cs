670158f33f45ce6c736c5c53747518a742deb81e
As1mov <realmofthemadzueira@gmail.com>
2025-01-28 17:08:41 -0300

    feat:adicionar URI 1002.

---
+++ URI 1002/URI 1002.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+//Code Written by Matt
+class URI {
+
+    static void Main(string[] args) { 
+        double A, raio, pi = 3.14159;
+
+        raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        
+        A = (raio * raio) * pi;
+        
+        A = Math.Round(A, 4);
+        
+        Console.WriteLine("A=" + A.ToString("0.0000", CultureInfo.InvariantCulture));
+    }
+
+}
