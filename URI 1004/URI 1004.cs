25f2e86007ea0666868168f3f96aab4737f79544
As1mov <realmofthemadzueira@gmail.com>
2025-01-30 20:24:58 -0300

    feat:adicionar o URI1004.

---
+++ URI 1004/URI 1004.cs
@@ -0,0 +1,17 @@
+﻿using System;
+//Code Written By Matt
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int A, B;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("PROD = " + (A*B));
+    }
+
+}
