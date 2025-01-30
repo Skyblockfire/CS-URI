36ee87f11a895c53d2a033f50e0575688989b4e9
As1mov <realmofthemadzueira@gmail.com>
2025-01-30 00:53:47 -0300

    feat:adicionar o URI1003.

---
+++ URI 1003/URI 1003.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//Code Written by Matt
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int A, B;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("SOMA = " + (A + B));
+    }
+
+}
