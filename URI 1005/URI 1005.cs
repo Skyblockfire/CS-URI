946e23ab333eac7754ebee8e5d83857bf276073d
As1mov <realmofthemadzueira@gmail.com>
2025-01-31 20:29:46 -0300

    feat:adicionar URI 1005.

---
+++ URI 1005/URI 1005.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+//Code Written By Matt
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture), B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("MEDIA = " + (((A*3.5)+(B*7.5))/11).ToString("0.00000", CultureInfo.InvariantCulture));
+
+    }
+
+}
