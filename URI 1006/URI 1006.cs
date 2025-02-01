39e08c42ad0b768f7bd1b176dac05b72e201a019
As1mov <realmofthemadzueira@gmail.com>
2025-02-01 19:55:26 -0300

    feat:adicionar o URI1006.

---
+++ URI 1006/URI 1006.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+//Code Written By Matt
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture), B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture), C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("MEDIA = " + (((A*2)+(B*3)+(C*5))/10).ToString("0.0", CultureInfo.InvariantCulture));
+    }
+}
