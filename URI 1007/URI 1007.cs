f47ee6387ed2e7adf34db7e3b390f82536a53ed4
As1mov <realmofthemadzueira@gmail.com>
2025-02-03 01:31:51 -0300

    feat:adicionar o URI1007.

---
+++ URI 1007/URI 1007.cs
@@ -0,0 +1,12 @@
+﻿using System;
+//Code Written By Matt
+class URI
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine()), B = int.Parse(Console.ReadLine()), C = int.Parse(Console.ReadLine()), D = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("DIFERENCA = " + (A * B - C * D));
+    }
+
+}
