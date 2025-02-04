798319713b31d92559e749e62742143f3a04f719
As1mov <realmofthemadzueira@gmail.com>
2025-02-04 20:39:12 -0300

    feat:adicionar o URI1008.

---
+++ URI 1008/URI 1008.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+//Code Written By Matt
+class URI
+{
+    static void Main(string[] args)
+    {
+        int ID = int.Parse(Console.ReadLine()), QtdHoras = int.Parse(Console.ReadLine());
+        double ValorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        string saida = "NUMBER = " + ID + "\n" + "SALARY = U$ " + (QtdHoras*ValorHora).ToString("0.00", CultureInfo.InvariantCulture);
+        
+        Console.WriteLine(saida);
+    }
+}
