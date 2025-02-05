e64c7b91c8fef76a487f4945b4a390ad134c1465
As1mov <realmofthemadzueira@gmail.com>
2025-02-04 21:34:04 -0300

    feat:adicionar o URI1009.

---
+++ URI 1009/URI 1009.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+//Code Written by Matt
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string Nome = Console.ReadLine();
+        double Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture), Comissao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture)*15/100;
+        Console.WriteLine("TOTAL = R$ " + (Salario + Comissao).ToString("F2", CultureInfo.InvariantCulture));
+    }
+
+}
