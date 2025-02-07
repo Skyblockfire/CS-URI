abab6beddcae955f623a875d858df2f2beb509de
As1mov <realmofthemadzueira@gmail.com>
2025-02-06 22:40:07 -0300

    feat:adicionar o URI1011.

---
+++ URI 1011/URI 1011.cs
@@ -0,0 +1,12 @@
+﻿using System;
+//Code Written By Matt
+class URI
+{
+    static void Main(string[] args)
+    {
+        double pi = 3.14159;
+        Int64 R = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("VOLUME = " + ((4/3.0) * pi * (R*R*R)).ToString("F3"));
+    }
+}
