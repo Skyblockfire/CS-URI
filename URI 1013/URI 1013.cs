a0dd95b2456b78ef75e97b5e33c4099a56f611bb
As1mov <realmofthemadzueira@gmail.com>
2025-02-11 03:53:31 -0300

    feat:adicionar o URI1013.

---
+++ URI 1013/URI 1013.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//Code Written By Matt
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        String Carlos = Console.ReadLine();
+        int A, B, C, Maior;
+
+        string[] valores = Carlos.Split(" ");
+        A = int.Parse(valores[0]);
+        B = int.Parse(valores[1]);
+        C = int.Parse(valores[2]);
+
+        Maior = (A + B + Math.Abs(A - B)) / 2;
+
+        Maior = (Maior + C + Math.Abs(Maior - C)) /2;
+
+        Console.WriteLine(Maior + " eh o maior");
+    }
+
+}
