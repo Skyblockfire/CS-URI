7062cef06c56572c93f7d0fd9ee8d8f81a892ee1
As1mov <realmofthemadzueira@gmail.com>
2025-02-12 05:06:59 -0300

    feat:adicionar o URI1014.

---
+++ URI 1014/URI 1014.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+//Code Written By Matt
+class URI
+{
+    static void Main(string[] args)
+    {
+        int Km = int.Parse(Console.ReadLine());
+        double litros = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine((Km/litros).ToString("F3") + " km/l");
+    }
+}
