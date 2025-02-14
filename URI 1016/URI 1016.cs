2434d8c0ea8fdfab492bc8b3b07f69c5e877a7fb
As1mov <realmofthemadzueira@gmail.com>
2025-02-14 03:34:58 -0300

    feat:aidionar o URI1016.

---
+++ URI 1016/URI 1016.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+//Code Written by Matt
+class URI
+{
+    static void Main(string[] args)
+    {
+        int dist = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(dist * 2 + " minutos");
+    }
+}
