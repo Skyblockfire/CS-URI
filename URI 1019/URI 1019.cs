9a400a181ebf6bd18de43c416bd13050e1837f5a
As1mov <realmofthemadzueira@gmail.com>
2025-02-20 08:27:43 -0300

    feat:adicionar o URI1019.

---
+++ URI 1019/URI 1019.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//Code Written By Matt
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int Hora=0, Minuto=0, Segundo=0;
+
+        Segundo = N;
+
+        Hora = Segundo / 3600;
+        Segundo %= 3600;
+
+        Minuto = Segundo / 60;
+        Segundo %= 60;
+        
+        Console.WriteLine($"{Hora}:{Minuto}:{Segundo}");
+    }
+}
