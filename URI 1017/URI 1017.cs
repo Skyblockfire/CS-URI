720efd748d9c2fd663874ac33d3f87efd3e6d7a4
As1mov <realmofthemadzueira@gmail.com>
2025-02-20 08:17:12 -0300

    feat:adicionar o URI1017.

---
+++ URI 1017/URI 1017.cs
@@ -0,0 +1,13 @@
+﻿using System;
+//Code Written by Matt
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double tempo = double.Parse(Console.ReadLine()), kmhora = double.Parse(Console.ReadLine());
+
+        Console.WriteLine((tempo * kmhora / 12).ToString("F3"));
+    }
+
+}
