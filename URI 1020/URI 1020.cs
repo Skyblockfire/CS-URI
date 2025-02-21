1a01602d7ea0e8bf46ffe551b1c326705a596523
As1mov <realmofthemadzueira@gmail.com>
2025-02-21 14:28:41 -0300

    feat:adicionar o URI1020.

---
+++ URI 1020/URI 1020.cs
@@ -0,0 +1,17 @@
+﻿using System;
+//Code Written By Matt
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int dias = int.Parse(Console.ReadLine()), anos, meses;
+
+        anos = dias/365;
+        dias %= 365;
+
+        meses = dias/30;
+        dias %= 30;
+
+        Console.WriteLine($"{anos} ano(s)\n{meses} mes(es)\n{dias} dia(s)");
+    }
+}
